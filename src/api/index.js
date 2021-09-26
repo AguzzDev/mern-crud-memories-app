86330698f6aa3aa21c16a8349eebffb75738272b
AguzZDev <example@example.com>
2021-09-26 13:12:31 -0300

    Change api host

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const url = "https://mern-crud-memories-app.herokuapp.com/posts"
+const url = process.env.API_URL
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
