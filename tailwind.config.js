0bd59d92610e9a286659edba7a5bf7c81e7311f7
AguzzDev <aribotta1901@gmail.com>
2023-11-16 11:56:59 -0300

    chore(package.json): remove and update talwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        Manrope: ["Manrope"]
+        Manrope: ["Manrope"],
       },
     },
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
